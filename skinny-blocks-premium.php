bb04009b35ba78a4af0de83c8913034627b13603
Michel Moraes <michel@nitdesign.com.br>
2020-06-23 00:29:39 +0200

    Including ACF PRO

---
+++ skinny-blocks-premium.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name:     Skinny Blocks Premium
+ * Description:     Premium Gutenberg blocks.
+ * Version:         0.1.0
+ * Author:          MRKWP
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     skinny-blocks-premium
+ *
+ * @package         MRKWP\SkinnyBlocksPremium
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+// Include the ACF plugin.
+require_once ( plugin_dir_path( __FILE__ ) . 'includes/acf/acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return plugin_dir_path( __FILE__ ) . 'includes/acf/acf.php';
+}
+
+
+/**
+ * TODO:
+ * Create Dependency Check
+ * Create a class-skinny-blocks for this plugin.
+*/
+
