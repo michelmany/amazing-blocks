3f3a02b197a7250aad06bd83a31a8c142686914d
Michel Moraes <michel@nitdesign.com.br>
2020-06-24 22:32:09 +0200

    Adding Custom Links for Archive Titles

--- post-types/newsletters/archive-sb-newsletter.php
+++ post-types/newsletters/archive-sb-newsletter.php
@@ -3,9 +3,27 @@ get_header();
 
 if ( have_posts() ) :
 	while ( have_posts() ) :
-		the_post();
-		the_content();
-	endwhile;
-endif;
+		the_post(); ?>
+
+		<?php
+			$newsletter_item_link = get_field( 'newsletter_item_link', 'option' );
+			$newsletter_type      = get_field( 'newsletter_type', $post->ID );
+			$pdf_file             = get_field( 'pdf_file', $post->ID );
+			$link_url             = get_field( 'link', $post->ID );
+			$download_link        = $newsletter_type === 'pdf' ? $pdf_file['url'] : $link_url['url'];
+			$link                 = $newsletter_item_link === 'item_page' ? get_permalink() : $download_link;
+		?>
+
+	 <h2>
+		 <a href="<?php echo $link; ?>"
+			 rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+				 <?php the_title(); ?>
+			</a>
+		</h2>
+
+		<?php // the_content(); ?>
+	 <?php endwhile; ?>
+	<?php
+ endif;
 
 get_footer();
