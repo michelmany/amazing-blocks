12adc64351a2128ed139e997387303cb6bed63d6
Michel Moraes <michel@nitdesign.com.br>
2020-06-24 22:42:29 +0200

    Archive Page amends

--- post-types/newsletters/archive-sb-newsletter.php
+++ post-types/newsletters/archive-sb-newsletter.php
@@ -1,29 +1,39 @@
 <?php
 get_header();
 
-if ( have_posts() ) :
-	while ( have_posts() ) :
-		the_post(); ?>
+?>
+<div style="text-align: center">
+	<h3>Archive: <?php post_type_archive_title(); ?></h3>
 
-		<?php
-			$newsletter_item_link = get_field( 'newsletter_item_link', 'option' );
-			$newsletter_type      = get_field( 'newsletter_type', $post->ID );
-			$pdf_file             = get_field( 'pdf_file', $post->ID );
-			$link_url             = get_field( 'link', $post->ID );
-			$download_link        = $newsletter_type === 'pdf' ? $pdf_file['url'] : $link_url['url'];
-			$link                 = $newsletter_item_link === 'item_page' ? get_permalink() : $download_link;
-		?>
-
-	 <h2>
-		 <a href="<?php echo $link; ?>"
-			 rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-				 <?php the_title(); ?>
-			</a>
-		</h2>
-
-		<?php // the_content(); ?>
-	 <?php endwhile; ?>
 	<?php
- endif;
 
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			?>
+
+			<?php
+				$newsletter_item_link = get_field( 'newsletter_item_link', 'option' );
+				$newsletter_type      = get_field( 'newsletter_type', $post->ID );
+				$pdf_file             = get_field( 'pdf_file', $post->ID );
+				$link_url             = get_field( 'link', $post->ID );
+				$download_link        = $newsletter_type === 'pdf' ? $pdf_file['url'] : $link_url['url'];
+				$link                 = $newsletter_item_link === 'item_page' ? get_permalink() : $download_link;
+			?>
+
+		<h4>
+			<a
+					href="<?php echo $link; ?>"
+					target="_blank"
+					title="Permanent Link to <?php the_title_attribute(); ?>">
+					<?php the_title(); ?>
+				</a>
+			</h4>
+
+		<?php endwhile; ?>
+	<?php endif; ?>
+
+</div>
+
+<?php
 get_footer();
